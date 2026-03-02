5d83d50b68380ae8e72dbd350279e68cc3e18667
Kamber Lovill <kambershayl@gmail.com>
2026-03-02 10:46:49 -0600

    adding in code comments

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+# prompt the user to input their name
 name = input("Enter your name: ")
+
+# say hello world
+print ("Hello world")
+
+# greet user
 print ("Hello " + name)
