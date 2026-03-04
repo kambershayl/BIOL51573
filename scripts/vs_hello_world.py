10d37914b519dc458c9b9eec831bc4f32e193bd0
Kamber Lovill <kambershayl@gmail.com>
2026-03-04 10:40:09 -0600

    script added

---
+++ scripts/vs_hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+# printing hello world
+print("Hello world")
+
+# prompt the user for their name
+name = input("enter your name: ")
+
+# print personal greeting using info from input()
+print("hello " + name + ", have a great day.")
+
+# some math
+print((7+7)*9)
