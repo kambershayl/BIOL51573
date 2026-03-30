ca534cc9e45249bde9d00e54b943abe7105755ef
Kamber Lovill <kambershayl@gmail.com>
2026-03-30 10:20:21 -0500

    uses argparse now

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 import argparse
+###------------- accept and parse command line arguments
+# create an argument parser object
+parser = argparse.ArgumentParser(description = "This script calculates the number at a given position \
+                                 in the Fibonacci sequence")
 
-#prompt the user for position in the Fibonacci sequence
-position = input("please enter a position:")
+# add a positional argument, in this case, the position in the Fibonacci sequence
+parser.add_argument("position", help = "Position in the Fibonacci sequence", type = int)
 
-#initialize two integers
+# prompt the user for position in the Fibonacci sequence
+# position = input("please enter a position:")
+
+#parse the arguments
+args = parser.parse_args()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# initialize two integers
 a,b = 0,1
 
-for i in range(int(position)):
+for i in range(int(args.position)):
     a,b = b, a + b
 
 fibonacci_number = a
